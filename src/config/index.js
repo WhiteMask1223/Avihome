1d957a1e1b1d100f8691c1f86cd639002123534f
White2312 <renielfernandez13@gmail.com>
2024-12-17 21:22:31 -0400

    fixes

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@
 export const SERVER_URL = process.env.SERVER_URL || `http://localhost:3000`;
 
 //DB url
-export const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017`;
+export const MONGODB_URI = process.env.MONGODB_URI || `mongodb+srv://White:White2312@avidb.p79pc.mongodb.net/?retryWrites=true&w=majority&appName=AviDB`;
 
 //NextAuth
 export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;

1133cd0b98957a610b418fa2a3a3781c6d247646
White2312 <renielfernandez13@gmail.com>
2024-12-18 17:32:26 -0400

    middleware fix 2

--- src/middleware.js
+++ src/middleware.js
@@ -2,15 +2,18 @@ import { NextResponse } from "next/server";
 
 export async function middleware(request) {
 
-    const isProduction = process.env.NODE_ENV === "production";
-    const cookieName = isProduction ? "__Secure-authjs.session-token:" : "authjs.session-token";
+    const isProduction = process.env.NODE_ENV === "Production";
+    const cookieName = isProduction ? "__Secure-authjs.session-token:" : "s-authjs.session-token";
+
+    console.log(cookieName)
 
 
     const sessionToken = request.cookies.get(cookieName);
 
     if (!sessionToken) {    
         // Redirige a /login si no está autenticado
-        return NextResponse.redirect(new URL("/", request.url));
+        console.log("Redirigido")
+        return NextResponse.redirect(new URL("/login", request.url));
     }
     // Permite el acceso si el usuario está autenticado
     return NextResponse.next();

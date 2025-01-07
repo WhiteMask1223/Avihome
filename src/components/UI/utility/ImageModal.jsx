01dec1b69100f185311a1f7b22775fd0e68f6dc0
White2312 <renielfernandez13@gmail.com>
2025-01-07 15:07:14 -0400

    minnor fix

--- src/components/UI/utility/ImageModal.jsx
+++ src/components/UI/utility/ImageModal.jsx
@@ -1,26 +1,10 @@
 import ModalSection from "./ModalSecction";
-import ReactImageMagnify from "react-image-magnify";
 
 export default function ImageModal({ trigger, setTrigger, imgUrl, selectedImg }) {
     if (trigger) return (
         <ModalSection trigger={trigger} setTrigger={setTrigger}>
 
             <div className="w-fit h-fit m-auto inset-0 absolute z-40 mt-32">
-                {/*<ReactImageMagnify 
-                    {...{
-                        smallImage: {
-                            alt: 'Imagen Completa',
-                            isFluidWidth: true,
-                            src: imgUrl[selectedImg].url || imgUrl[selectedImg],
-                        },
-                        largeImage: {
-                            src: imgUrl[selectedImg].url || imgUrl[selectedImg],
-                            width: 1920,
-                            height: 600,
-                        },
-                        enlargedImagePosition: 'over',
-                    }}
-                />*/}
                 <img
                     className=""
                     src={imgUrl[selectedImg].url || imgUrl[selectedImg]} alt="IMG"
